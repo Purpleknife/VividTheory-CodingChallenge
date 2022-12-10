67bae59513daba328acc1e1501cbde8dad48e516
Hind <hind.nayhi@gmail.com>
2022-12-09 21:42:36 -0700

    Add route to server.

--- back-end/dist/server.js
+++ back-end/dist/server.js
@@ -3,13 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// load .env data into process.env
+// Load .env data:
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const db = require('./connection');
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+// Express Configuration
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use(body_parser_1.default.json());
+// Routes
+const blogRoutes = require('./routes/blogs');
+app.use('/', blogRoutes(db));
 app.listen(port, () => {
     console.log(`Express is listening on port ${port}!`);
 });
--- back-end/server.ts
+++ back-end/server.ts
@@ -4,10 +4,19 @@ dotenv.config();
 
 const db = require('./connection');
 import express, { Express } from 'express';
+import bodyParser from 'body-parser';
 
 const app: Express = express();
 const port = process.env.PORT || 8080;
 
+// Express Configuration
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Routes
+const blogRoutes = require('./routes/blogs');
+app.use('/', blogRoutes(db));
 
 app.listen(port, () => {
   console.log(`Express is listening on port ${port}!`);
