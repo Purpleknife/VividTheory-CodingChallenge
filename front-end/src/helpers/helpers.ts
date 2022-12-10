49c573b103854c7d9f5167863b32459025eaeb00
Hind <hind.nayhi@gmail.com>
2022-12-10 11:06:05 -0700

    Implement a helper function to get an array from the total number of pages.

---
+++ front-end/src/helpers/helpers.ts
@@ -0,0 +1,15 @@
+
+/*
+Helper function that returns an array depending on the total of pages,
+and the minimun page limit, which is normally 1.
+*/
+
+export const createAPageArray = (minPageLimit: number, totalPages: number) => {
+  const pages: number[] = [];
+
+  for (let i = minPageLimit; i <= totalPages; i++) {
+    pages.push(i);
+  }
+
+  return pages;
+};
