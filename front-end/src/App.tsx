9b4d4548fefd24856bc0b3b756ca6aa9776bfa3f
Hind <hind.nayhi@gmail.com>
2022-12-10 13:02:14 -0700

    Add route for a single blog page.

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 
 import BlogSearchPage from './components/BlogSearchPage';
+import SingleBlogPage from './components/SingleBlogPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<BlogSearchPage />} />
+        <Route path='/:slug' element={<SingleBlogPage />} />
       </Routes>
     </BrowserRouter>
   );
