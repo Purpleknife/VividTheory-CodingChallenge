6804100d43abcd4240c33106fd212520e3d5a609
Hind <hind.nayhi@gmail.com>
2022-12-09 19:12:35 -0700

    Setup our initial components.

---
+++ front-end/src/components/BlogSearchPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const BlogSearchPage = () => {
+  return (
+    <div>
+      Hello from BlogSearchPage!
+    </div>
+  );
+}
+ 
+export default BlogSearchPage;
---
+++ front-end/src/components/SingleBlogPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const SingleBlogPage = () => {
+  return (
+    <div>
+      Hello from SingleBlogPage!
+    </div>
+  );
+}
+
+export default SingleBlogPage;
